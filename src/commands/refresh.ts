bf16e71098c8851438d19eae2557250f9ca2f938
onivoid <skullyfox.yt@gmail.com>
2024-05-05 13:42:34 +0200

    Member count fixe

--- src/commands/refresh.ts
+++ src/commands/refresh.ts
@@ -6,8 +6,10 @@ export default {
   execute: async (interaction: CommandInteraction) => {
     const guildId = '267312518463094788';
     const guild = await interaction.client.guilds.cache.get(guildId);
+    if (!guild) return interaction.reply('Impossible de trouver le serveur');
+    const memberCount = (await guild.members.fetch()).size;
     const activity: ActivityOptions = {
-      name: `${guild?.memberCount} membres 👀`,
+      name: `${memberCount} membres 👀`,
       type: ActivityType.Watching,
     };
     interaction.reply('Compteur de membres redémarré');
